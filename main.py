58e197dc8f0b385470df6db1789490b837ba0707
rgb_beard <62804472+rgbbeard@users.noreply.github.com>
2022-05-28 09:27:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,37 @@
-import win_webdriver
-import win_gui
-import win_system
+from threadmaid import ThreadMaid
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+
+
+driver_config = webdriver.FirefoxOptions()
+driver_config.binary_location = "geckodriver.exe"
+driver = webdriver.Firefox()
+
+
+def start():
+	global driver
+
+	driver.get("https://web.whatsapp.com")
+
+
+	while True:
+		login_page = False
+
+		try:
+			login_page = driver.find_element(by=By.CLASS_NAME, value="b77wc")
+		except Exception as i:
+			print("waiting for login screen..")
+
+		if login_page:
+			while login_page:
+				print("waiting for login..")
+				sleep(5)
+		else:
+			print("login succeeded")
+			exit()
+		sleep(1)
+
+
+main_thread = ThreadMaid().setup(start)
+main_thread.run()
